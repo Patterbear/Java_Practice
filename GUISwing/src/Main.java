7410db0bc6b20f0e81963e8061e9c5f91a0d5dce
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-12-29 18:34:34 +0000

    labels

--- GUISwing/src/Main.java
+++ GUISwing/src/Main.java
@@ -1,23 +1,41 @@
 import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-//		JFrame frame = new JFrame(); //Creates a frame
-//		frame.setTitle("JFrame Title"); //Sets title of frame
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets close operation to kill the window
-//		frame.setResizable(false); //Fixes size of window
-//		frame.setSize(420, 420); //Sets x and y dimension of frame
-//		frame.setVisible(true); //Makes frame visible
-//		
-//		ImageIcon image = new ImageIcon("src/logo.png"); //Creates ImageIcon object
-//		frame.setIconImage(image.getImage()); //Sets frame's icon
-//		frame.getContentPane().setBackground(Color.lightGray); //Changes colour of background
-
-		MyFrame myFrame = new MyFrame();
+		ImageIcon image = new ImageIcon("src/building.JPG"); //Creates ImageIcon object
+		Border border = BorderFactory.createLineBorder(Color.green, 3); //Creates Border object
+		
+		JLabel label = new JLabel(); //Creates a JLabel object
+		label.setText("The Fielding Johnson Building"); //Sets text of label
+		label.setIcon(image); //Adds image to label
+		label.setHorizontalTextPosition(JLabel.CENTER); //Sets horizontal position of text (LEFT/CENTER/RIGHT)
+		label.setVerticalTextPosition(JLabel.TOP); //Sets vertical position of text (TOP/BOTTOM/CENTER)
+		label.setForeground(Color.GREEN); //Sets colour of text
+		label.setFont(new Font("Segoe UI", Font.PLAIN, 20)); //Sets font of text
+		label.setIconTextGap(12); //Sets distance between text and image
+		label.setBackground(Color.BLACK); //Set background colour
+		label.setOpaque(true); //Display background colour
+		label.setBorder(border); //Sets label border
+		label.setVerticalAlignment(JLabel.CENTER); //Sets vertical alignment of label
+		label.setHorizontalAlignment(JLabel.CENTER); //Sets horizontal alignment of label
+		//label.setBounds(100, 100, 350, 350); //Sets x and y position of label in frame as well as its dimensions
+		
+		JFrame frame = new JFrame(); //Creates a frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets close operation to kill the window
+		frame.setSize(500, 500); //Sets x and y dimension of frame
+		//frame.setLayout(null); //Sets layout manager to 'null'
+		frame.setVisible(true); //Makes frame visible
+		frame.add(label); //Adds label to frame
+		frame.pack(); //Resizes frame to accomodate all elements within it (must be done after all elements added)
 	}
 
 }
