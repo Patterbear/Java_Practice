74e5d6292ead8ee410318d4b1c0f6bd7ddb75ccb
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2021-01-01 12:06:36 +0000

    buttons

--- GUISwing/src/Main.java
+++ GUISwing/src/Main.java
@@ -1,48 +1,10 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ImageIcon icon = new ImageIcon("flag.png"); //ImageIcon object created
-		
-		JLabel label = new JLabel(); //Creates JLabel object
-		label.setText("Label Text"); //Sets label text
-		label.setIcon(icon); //Sets the icon of the label
-		//label.setVerticalAlignment(JLabel.BOTTOM); //Sets vertical alignment of label
-		//label.setHorizontalAlignment(JLabel.RIGHT); //Sets vertical alignment of label
-		label.setBounds(100, 100, 75, 75); //Sets dimensions and coordinates of label
-		
-		JPanel redPanel = new JPanel(); //Creates JPanel object
-		redPanel.setBackground(Color.RED); //Sets background colour of panel
-		redPanel.setBounds(0, 0, 250, 250); //Sets panel dimensions and coordinates
-		redPanel.setLayout(null); //Sets layout manager to a new BorderLayout object
-		
-		JPanel bluePanel = new JPanel(); //Creates JPanel object
-		bluePanel.setBackground(Color.BLUE); //Sets background colour of panel
-		bluePanel.setBounds(250, 0, 250, 250); //Sets panel dimensions and coordinates
-		bluePanel.setLayout(null); //Sets layout manager to a new BorderLayout object
-		
-		JPanel greenPanel = new JPanel(); //Creates JPanel object
-		greenPanel.setBackground(Color.GREEN); //Sets background colour of panel
-		greenPanel.setBounds(0, 250, 500, 250); //Sets panel dimensions and coordinates
-		greenPanel.setLayout(null); //Sets layout manager to a new BorderLayout object
-		
-		JFrame frame = new JFrame(); //Creates JFrame object
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets close operation
-		frame.setLayout(null); //Sets layout manager to 'null'
-		frame.setSize(750, 750); //Sets dimensions of frame
-		frame.setVisible(true); //Makes frame visible
-		greenPanel.add(label); //Adds label to panel
-		frame.add(redPanel); //Adds panel to frame
-		frame.add(bluePanel); //Adds panel to frame
-		frame.add(greenPanel);  //Adds panel to frame
+		new MyFrame(); //Creates object of type 'MyFrame'
 		
 	}
 
--- GUISwing/src/MyFrame.java
+++ GUISwing/src/MyFrame.java
@@ -1,20 +1,68 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class MyFrame extends JFrame {
-	MyFrame() {
+public class MyFrame extends JFrame implements ActionListener {
 		
-		this.setTitle("JFrame Title"); //Sets title of this
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets close operation to kill the window
-		this.setResizable(false); //Fixes size of window
-		this.setSize(420, 420); //Sets x and y dimension of this
-		this.setVisible(true); //Makes this visible
+		JButton button; //Declares 'button' object as a global variable
+		JLabel label; //Declares 'label' object as a global variable
+		int buttonPressed = 0; //Declares 'buttonPressed' variable as a global variable with value '0'
+	
+		MyFrame() { //Constructor method
+			
+			ImageIcon icon = new ImageIcon("flag.png"); //Creates ImageIcon object
+			ImageIcon icon2 = new ImageIcon("flag2.png"); //Creates ImageIcon object
+			
+			label = new JLabel(); //Creates JLabel object
+			label.setIcon(icon2); //Sets label icon
+			label.setBounds(150, 250, 150, 150); //Sets dimensions and coordinates of label
+			label.setVisible(false); //Sets label visibility to 'false'
+			
+			button = new JButton(); //Creates JButton object
+			button.setBounds(100, 100, 250, 100); //Sets dimensions and coordinates of button
+			button.addActionListener(this); //Adds ActionListener to button
+			//button.addActionListener(e -> System.out.println("Button Pressed.")); //Assigning button function using lambda
+			button.setText("Button"); //Sets text on button
+			button.setFocusable(false); //Removes text outline
+			button.setIcon(icon); //Sets button icon
+			button.setHorizontalTextPosition(JButton.CENTER); //Sets horizontal text position
+			button.setVerticalTextPosition(JButton.BOTTOM); //Sets vertical text position
+			button.setFont(new Font("Segoe UI", Font.BOLD, 25)); //Sets font of button text
+			button.setIconTextGap(-5); //Sets size of gap between icon and text
+			button.setForeground(Color.GRAY); //Selects colour of text
+			button.setBackground(Color.WHITE); //Sets colour of button
+			button.setBorder(BorderFactory.createEtchedBorder()); //Sets button border
+			//button.setEnabled(false); //Disables button
+			
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets default close operation
+			this.setLayout(null); //Sets layout manager to 'null'
+			this.setSize(500, 500); //Sets dimensions of frame
+			this.setVisible(true); //Sets visibility to 'true'
+			this.add(button); //Adds button to frame
+			this.add(label); //Adds label to frame
+			
+		}
 		
-		ImageIcon image = new ImageIcon("src/logo.png"); //Creates ImageIcon object
-		this.setIconImage(image.getImage()); //Sets this's icon
-		this.getContentPane().setBackground(Color.lightGray); //Changes colour of background
-
-	}
+		@Override //Override decorator
+		public void actionPerformed(ActionEvent e) { //Method to assign button function
+			if(e.getSource()==button) { //If button is pressed the below code executes
+				//System.out.println("Button pressed."); //Text outputted to console
+				//button.setEnabled(false); //Disables button
+				if(buttonPressed % 2 ==0) {
+					label.setVisible(true); //Makes label visible
+				} else {
+					label.setVisible(false); //Makes label invisible
+				}
+				
+				buttonPressed++; //Increases integer variable by 1
+				
+			}
+		}
 }
